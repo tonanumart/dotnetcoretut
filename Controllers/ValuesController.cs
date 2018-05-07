646b718fe2aa5a51fed65fa9666e56dc71a1bcd4
MY-PC <MY-PC>
2018-05-07 22:14:21 +0700

    Initail Commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly DataContext contextDb;
+
+        public ValuesController(DataContext context)
+        {
+            this.contextDb = context;
+        }
+
+
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await contextDb.Values.ToListAsync();
+            //throw new NotImplementedException("Please comment this code.");
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var result = await contextDb.Values.FirstOrDefaultAsync(x=>x.Id == id);
+            return Ok(result);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
